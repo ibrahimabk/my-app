fac388a79e137e2f232c8fa7a0d6973a50ef3599
ibrahimabk <47291466+ibrahimabk@users.noreply.github.com>
2019-03-31 22:40:01 +0300

    Update DatabaseHelper.java

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -1,4 +1,3 @@
-
 package com.example.login;
 
 import android.content.ContentValues;
@@ -7,58 +6,47 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+/**
+ * Created by Ibrahim on 31/03/2019.
+ */
+
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "Register.db";
-    public static final String TABLE_NAME = "Register";
-    public static final String COL_1 = "FirstName";
-    public static final String COL_2 = "Username";
-    public static final String COL_3 = "Email";
-    public static final String COL_4 = "Password";
-
-    public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+    public DatabaseHelper(Context context){
+        super(context, Database.DATABASE_NAME, null, Database.VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE Register(FirstName TEXT, Username TEXT,Email TEXT,Password TEXT)");
-
+    public void onCreate(SQLiteDatabase db) {
+        //db.execSQL("DROP TABLE student;");
+        db.execSQL(Database.Student.CREATE_QUERY);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(sqLiteDatabase);
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
     }
 
-    public long addUseer(String user, String password,String mail,String fname) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValue = new ContentValues();
-        contentValue.put("FirstName", fname);
-        contentValue.put("Username", user);
-        contentValue.put("Email", mail);
-        contentValue.put("Password", password);
-
-
-        Long rest = db.insert("Register", null, contentValue);
-        db.close();
-        return rest;
+    //Method to insert values to the tables
+    public long insert(String table, ContentValues values){
+        return getWritableDatabase().insert(table, null, values);
     }
 
-    public boolean checkUser(String Username, String Password) {
-       String[] columns = {COL_1, COL_3};
-        SQLiteDatabase db = getReadableDatabase();
-        String selection = COL_2 + "=?" + " and " + COL_4 + "=?";
-        String[] selectionArgs = {Username, Password};
-        Cursor cursor = db.query(TABLE_NAME,columns, selection, selectionArgs, null, null, null);
-        int count = cursor.getCount();
-        cursor.close();
-        db.close();
-        if (count > 0)
-            return true;
-        else
-            return false;
+    //Method to update values in the tables
+    public int update(String table, ContentValues values, String whereClause, String[] whereArgs){
+        return getWritableDatabase().update(table, values, whereClause, whereArgs);
+    }
 
+    //Method to delete records from the tables
+    public int delete(String table, String whereClause, String[] whereArgs){
+        return getWritableDatabase().delete(table, whereClause, whereArgs);
+    }
+
+
+    //Method to read records from the tables
+    public Cursor read(String table, String columns[], String whereClause, String[] whereArgs,
+                       String groupByClause, String havingClause, String orderByClause, String limit){
+        return getReadableDatabase().query(table, columns, whereClause, whereArgs, groupByClause,
+                havingClause, orderByClause, limit);
     }
 }
